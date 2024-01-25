80025d46e8cfcf12bcb4edef1f5670f8f6c7dfa6
elliotjarnit <me@elliotjarnit.dev>
2024-01-25 09:04:15 -0800

    start

--- src/main/java/dev/elliotjarnit/elliotstrike/Main.java
+++ src/main/java/dev/elliotjarnit/elliotstrike/Main.java
@@ -1,7 +1,35 @@
 package dev.elliotjarnit.elliotstrike;
 
-public class Main {
+import dev.elliotjarnit.elliotengine.ElliotEngine;
+
+public class Main extends ElliotEngine {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main().run();
+    }
+
+    @Override
+    public void optionSetup() {
+        setOption(Options.NAME, "Elliot Strike");
+        setOption(Options.AUTHOR, "Elliot Jarnit-Bjergsoe");
+        setOption(Options.DESCRIPTION, "A 3D FPS game made with ElliotEngine");
+        setOption(Options.VERSION, getClass().getPackage().getImplementationVersion());
+        setOption(Options.WINDOW_WIDTH, String.valueOf(1280));
+        setOption(Options.WINDOW_HEIGHT, String.valueOf(720));
+        // Check if running from jar
+        if (this.getClass().getResource("Main.class").toString().startsWith("jar")) {
+            this.setOption(Options.LOADING_SCREEN, "true");
+        } else {
+            this.setOption(Options.LOADING_SCREEN, "false");
+        }
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void loop() {
+
     }
 }
